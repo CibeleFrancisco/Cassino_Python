1b41ae0cc062a2a6d2e53249430bc8b5ac665381
Cibele Francisco <cibelefrancisco1@gmail.com>
2021-05-18 12:00:30 +0100

    Update

--- JO KEN PO.py
+++ JO KEN PO.py
@@ -1,31 +1,41 @@
-"""Exercício Python 45: Crie um programa que faça o computador jogar Jokenpô com você."""
 import random
-user = int(input("""Escolha entre as opções: 
-1 - pedra
-2 - papel
-3 - tesoura:  """))
-if user ==1:
-    print(f'Você escolheu pedra')
-elif user ==2:
-    print("Você escolheu papel")
-elif user ==3:
-    print("Você escolheu tesousa")
-else:
-    print("\33[7:31:40mValor inválido!\33[m")
-comp = random.randint(1,3)
-if comp==1:
-    print("O Computador escolheu pedra")
-elif comp==2:
-    print("O computador escolheu papel")
-elif comp==3:
-    print("O computador escolheu tesoura")
-if user ==comp:
-    print("Empate!")
-elif user == 3 and comp ==1:
-    print("Computador venceu!")
-elif user ==1 and comp ==3:
-    print("Você venceu!")
-elif user < comp:
-    print("Você venceu!!")
-elif comp < user:
-    print("Computador venceu")
+from time import sleep
+rep= "Y"
+while rep == "Y":
+    user = int(input("""Choose: 
+    [1] - ROCK
+    [2] - PAPER
+    [3] - SCISSORS:  
+     """))
+    if user ==1:
+        print(f'You chose ROCK')
+    elif user ==2:
+        print("You chose PAPER")
+    elif user ==3:
+        print("You chose SCISSORS")
+    else:
+        print("\33[7:31:40mINVALID OPTION!\33[m")
+    print("JO")
+    sleep(1)
+    print("KEN")
+    sleep(1)
+    print("PO!")
+    sleep(1)
+    comp = random.randint(1,3)
+    if comp==1:
+        print("I chose ROCK")
+    elif comp==2:
+        print("I You chose PAPER")
+    elif comp==3:
+        print("I You chose SCISSORS")
+    if user ==comp:
+        print("TIE!")
+    elif user == 3 and comp ==1:
+        print("I WIN!")
+    elif user ==1 and comp ==3:
+        print("YOU WIN! CONGRATULATIONS!")
+    elif user < comp:
+        print("YOU WIN! CONGRATULATIONS!")
+    elif comp < user:
+        print("I WIN!")
+
